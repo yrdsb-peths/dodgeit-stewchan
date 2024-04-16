e783a95c9c309646bd7c9994ab468a741e7509c9
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-04-16 09:04:59 -0400

    Reset banana

--- Banana.java
+++ Banana.java
@@ -4,6 +4,20 @@ public class Banana extends Actor
 {
     public void act()
     {
-        move(-3);
+        move(-10);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
+        
     }
 }
