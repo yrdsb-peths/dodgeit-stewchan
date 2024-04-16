723c8e07c9c8a60397db3c6b33fcf7674ea3ea3e
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-04-16 09:02:28 -0400

    Add banana

--- MyWorld.java
+++ MyWorld.java
@@ -9,5 +9,8 @@ public class MyWorld extends World
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        Banana banana = new Banana();
+        addObject(banana, 600, 100);
     }
 }
