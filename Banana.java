379b1c11d1510ea6c3961fab7a872e4f354dfb5c
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-04-16 09:07:21 -0400

    Add sad face

--- Banana.java
+++ Banana.java
@@ -9,6 +9,13 @@ public class Banana extends Actor
         if(getX() <= 0) {
             resetBanana();
         }
+        
+        if(isTouching(Hero.class)){
+            // Add a gameover symbol
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
     
     public void resetBanana() {
