ad4947eb55003ab7ab3e5882fad13b2afd423061
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-04-16 09:00:52 -0400

    Add Hero

---
+++ Hero.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Hero extends Actor
+{
+    boolean atTop = true;
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(null)) {
+            atTop = !atTop;
+        }
+        
+        if(atTop) {
+            setLocation(100, 100);
+        } else {
+            setLocation(100, 300);
+        }
+    }
+}
