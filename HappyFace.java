b2c027f2f0fc7efb01e1fc5f3bb539c8ac336668
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-04-29 09:05:36 -0400

    Make happy face move

--- HappyFace.java
+++ HappyFace.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class HappyFace here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class HappyFace extends Actor
 {
     /**
@@ -14,6 +9,6 @@ public class HappyFace extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        move(2);
     }
 }
